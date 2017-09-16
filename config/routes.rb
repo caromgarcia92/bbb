78b98dfb70560e9cbf0052471d4596af9c4c2d11
Sam Abbassi <samabbassi1@gmail.com>
2017-09-16 16:26:17 -0400

    updated the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
   resources :reviews
   resources :pictures
+  resources :users
+  resources :restaurants
+
 
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
-  resources :users
+  # get 'restaurants/index'
 
-  get 'restaurants/index'
 
-  resources :restaurants
-
-  root 'restaurants#index'
+  # root 'restaurants#index'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,19 @@ ActiveRecord::Schema.define(version: 20170916175257) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.integer "busyness"
+    t.string "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -30,22 +43,6 @@ ActiveRecord::Schema.define(version: 20170916175257) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
-
-
-
-  create_table "restaurants", force: :cascade do |t|
-    t.string "name"
-    t.integer "busyness"
-    t.string "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.string "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
