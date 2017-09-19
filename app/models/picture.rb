5c8a6d06716b045321733989c75c91b502c88991
Sam Abbassi <samabbassi1@gmail.com>
2017-09-19 16:43:39 -0400

    show picture now working

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,7 @@
 class Picture < ApplicationRecord
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\z/
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*/
 
   # belongs_to :user
   belongs_to :restaurant
